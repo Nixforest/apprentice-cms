802f733c2863adf4d54a9a35d2e33b170921f144
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@ce5f66a6-c26f-081e-5279-c115e52cd7a7>
2011-10-18 16:47:55 +0000

    Đã tạo Module Manager, mặc dù chưa hoàn thành, nhưng đã có tính năng Load List Module. Mọi người xem lại trong file application/Bootstrap.php, có một hàm kết nối với database, mọi người sẽ dùng tạm, chúng ta sẽ có một lớp thư viện kết nối database sau.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,28 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+		protected function _initDb(){
+		
+		$optionResources = $this->getOption('resources');
+		$dbOption = $optionResources['db'];
+		$dbOption['params']['username'] = 'apprentice';
+		$dbOption['params']['password'] = 'apprentice';
+		$dbOption['params']['dbname'] = 'apprentice_cms';
+		
+		$adapter = $dbOption['adapter'];
+		$config = $dbOption['params'];
+		
+		$db = Zend_Db::factory($adapter,$config);
+		$db->setFetchMode(Zend_Db::FETCH_ASSOC);
+		$db->query("SET NAMES 'utf8'");
+		$db->query("SET CHARACTER SET 'utf8'");
+		
+		Zend_Registry::set('connectDb',$db);
+		Zend_Db_Table::setDefaultAdapter($db);
+		
+		return $db;
+		
+	}
 
 }
 
