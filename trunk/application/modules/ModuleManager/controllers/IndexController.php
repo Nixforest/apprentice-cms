b25ffedfabefb58fbb2568c11638bd9d79c70a27
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@ce5f66a6-c26f-081e-5279-c115e52cd7a7>
2011-10-19 16:18:47 +0000

    Khởi tạo admin module, là module quản lý những chức năng bên trong backend. Các bạn chạy file upload/apprentice_cms.sql để tạo table trong database trước nhé!

--- trunk/application/modules/ModuleManager/controllers/IndexController.php
+++ trunk/application/modules/ModuleManager/controllers/IndexController.php
@@ -4,10 +4,35 @@ class ModuleManager_IndexController extends Zend_Controller_Action{
 		
 	}
 	
-	public function listAction(){
+	public function listAction(){		
+		if(isset($_POST['btnsubmit'])){
+			if($_FILES['file']!=null){				
+				move_uploaded_file($_FILES['file']['tmp_name'], APPLICATION_PATH."/modules/".$_FILES['file']['name']);
+				
+				$filter = new Zend_Filter_Decompress(array(
+					'adapter' => 'zip',
+					'option' => array(
+						'target' => 'C:\Temp')));
+				/*$options = array(
+				   'adapter' => 'zip',
+				   'options' => array(
+				            'target' => APPLICATION_PATH."/modules/",
+				            ),
+				      );
+				$filter = new Zend_Filter_Decompress($options);*/
+				
+				$filter->filter(APPLICATION_PATH."/modules/".$_FILES['file']['name']);
+				
+				echo APPLICATION_PATH."/modules/".$_FILES['file']['name'];
+				$query1 = "insert into modulemanager (module_id,name,description,thumbnail,author, email,version,license) values('3','contact','Contact module', '','Nixforest', 'Nixforest21991920@gmail.com','1.0.0','free')";
+				$db = Zend_Registry::get('connectDb');
+				$result = $db->query($query1);
+			}
+		}
 		$query = "select * from modulemanager;";
 		$db = Zend_Registry::get('connectDb');
 		$result = $db->query($query);
-		$this->view->result=$result;
+		$this->view->result=$result;	
 	}
+		
 }
