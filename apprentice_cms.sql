2945694d7d004444a15f6d47bb02af42835d6b46
qconguit@gmail.com <qconguit@gmail.com@ce5f66a6-c26f-081e-5279-c115e52cd7a7>
2011-10-20 15:28:00 +0000

    Cập nhật LoginController vào trong module admin.

---
+++ apprentice_cms.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 18, 2011 at 06:53 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `apprentice_cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modulemanager`
+--
+
+CREATE TABLE IF NOT EXISTS `modulemanager` (
+  `module_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `thumbnail` text NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `version` varchar(10) NOT NULL,
+  `license` varchar(10) NOT NULL,
+  PRIMARY KEY (`module_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `modulemanager`
+--
+
+INSERT INTO `modulemanager` (`module_id`, `name`, `description`, `thumbnail`, `author`, `email`, `version`, `license`) VALUES
+(1, 'Module Manager', 'Manager Modules', '', 'Nixforest', 'Nixforest21991920@gmail.com', '1.0.0', 'free'),
+(2, 'default', 'Default module', '', 'Nixforest', 'Nixforest21991920@gmail.com', '1.0.0', 'free');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
