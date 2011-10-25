9d60851d1d4586535247b02476db105f172226aa
huyuit@gmail.com <huyuit@gmail.com@ce5f66a6-c26f-081e-5279-c115e52cd7a7>
2011-10-25 12:06:55 +0000

    Đã giải nén tự động file .zip khi add 1 module vào CMS và tự động xóa file .zip sau khi đã giải nén thành công.

--- application/modules/ModuleManager/controllers/IndexController.php
+++ application/modules/ModuleManager/controllers/IndexController.php
@@ -55,22 +55,24 @@ class ModuleManager_IndexController extends Zend_Controller_Action{
 		if(isset($_POST['btnsubmit'])){
 			if($_FILES['file']!=null){				
 				move_uploaded_file($_FILES['file']['tmp_name'], APPLICATION_PATH."/modules/".$_FILES['file']['name']);
-				
+				/*
 				$filter = new Zend_Filter_Decompress(array(
 					'adapter' => 'zip',
 					'option' => array(
-						'target' => 'C:\Temp')));
-				/*$options = array(
-				   'adapter' => 'zip',
-				   'options' => array(
-				            'target' => APPLICATION_PATH."/modules/",
-				            ),
-				      );
-				$filter = new Zend_Filter_Decompress($options);*/
+						'target' => APPLICATION_PATH.'/modules'),));
+				*/
+				//$filter->filter(APPLICATION_PATH."/modules/".$_FILES['file']['name']);
 				
-				$filter->filter(APPLICATION_PATH."/modules/".$_FILES['file']['name']);
-				
-				echo APPLICATION_PATH."/modules/".$_FILES['file']['name'];
+				$filter     = new Zend_Filter_Decompress(array('adapter' => 'Zip','options' => array('target' => APPLICATION_PATH.'/modules',)));
+				$testUncompressed = $filter->filter(APPLICATION_PATH.'/modules/'.$_FILES['file']['name']);
+				if ($testUncompressed)
+				{
+					unlink(APPLICATION_PATH.'/modules/'.$_FILES['file']['name']);
+				}
+				else 
+				{
+					$this->view->ErrorMessage=true;
+				}
 				$query1 = "insert into modulemanager (module_id,name,description,thumbnail,author, email,version,license) values('3','contact','Contact module', '','Nixforest', 'Nixforest21991920@gmail.com','1.0.0','free')";
 				$db = Zend_Registry::get('connectDb');
 				$result = $db->query($query1);
