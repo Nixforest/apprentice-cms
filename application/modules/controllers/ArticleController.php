83ae7560daf7e94e6ce633ed94612b00dce65800
greatpearl22@gmail.com <greatpearl22@gmail.com@ce5f66a6-c26f-081e-5279-c115e52cd7a7>
2011-10-31 18:31:38 +0000

    Sửa lổi không hiển thị ảnh khi load editor Chuyển hàm khởi tạo sang bootstrap Bổ xung about, uninstall

---
+++ application/modules/controllers/ArticleController.php
@@ -0,0 +1,96 @@
+<?php
+class News_ArticleController extends Zend_Controller_Action {
+	
+	public function addAction() {
+		$request = $this->getRequest();
+		if ($request->isPost()) {
+			$article = new Model_ArticleDTO();
+			$article->setData(array(
+				'title'         => strip_tags($request->getPost('title')),
+				'sub_title'     => strip_tags($request->getPost('subTitle')),
+				'slug'          => $request->getPost('slug'),
+			    'description'   => $request->getPost('description'),
+			    'content'       => $request->getPost('content'),
+				'author'        => strip_tags($request->getPost('author')),
+				'allow_comment' => $request->getPost('allowComment'),
+				'is_hot'        => $request->getPost('hotArticle')
+			));
+			
+			$news = new Model_NewsBLO();
+			$id = $news->addNews($article);
+			
+			$this->view->message = "Add a new article successful.";
+		}
+	}
+
+//DUONG THAN DAN
+	public function listAction(){
+		//$module = new Model_News();
+		$module = new Model_NewsBLO();
+		$result=$module->getAll();
+		$this->view->result=$result;
+	}
+	
+	public function editAction(){
+		include '../application/modules/news/forms/editForm.php';
+		$form = new Form_Edit();
+		//$form->setAction('../News/edit/');
+		$form->setMethod('post');
+		
+		if($this->getRequest()->isPost()){
+			if($form->isValid($_POST)){
+				$article = new Model_ArticleDTO();
+				$article->set('article_id', $form->getValue('article_id'));
+				$article->set('title', $form->getValue('title'));
+				$article->set('description', $form->getValue('description'));
+				
+				$newsBlo = new Model_NewsBLO();
+				$newsBlo->updateNews($article);
+				
+				$this->_redirect('news/article/list');
+			}
+		}
+		else{
+			$article_id = $this->_request->getParam('id');
+			
+			$module = new Model_NewsBLO();
+			$newsEdit = $module->getById($article_id);
+			
+			$newsEdit->setFetchMode(Zend_Db::FETCH_NUM);
+			if($row = $newsEdit->fetchAll()){
+				$form->title->setValue($row[0][1]);
+				$form->description->setValue($row[0][2]);
+				$form->article_id->setValue($row[0][0]);		
+			}
+		}
+		
+		$this->view->form = $form;
+	}
+	
+	public function deleteAction(){
+		$article_id = $this->_request->getParam('id');
+		
+		$module = new Model_NewsBLO();
+		$module->deleteNews($article_id);
+		
+		$this->_redirect('news/article/list');
+	}
+	
+	public function activeAction(){
+		$article_id = $this->_request->getParam('id');
+		
+		$module = new Model_NewsBLO();
+		$module->changeStatus($article_id, 'active');
+		
+		$this->_redirect('news/article/list');
+	}
+	
+	public function deactiveAction(){
+		$article_id = $this->_request->getParam('id');
+		
+		$module = new Model_NewsBLO();
+		$module->changeStatus($article_id, 'inactive');
+		
+		$this->_redirect('news/article/list');
+	}
+}
