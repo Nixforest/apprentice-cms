ec96f85664518e738e59470211bc0b09e0e23425
greatpearl22@gmail.com <greatpearl22@gmail.com@ce5f66a6-c26f-081e-5279-c115e52cd7a7>
2012-01-13 15:25:10 +0000

    Commit News Again

---
+++ trunk/application/modules/news/models/ArticleDTO.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @author Nguyen Ngoc Dai.
+ * @uses ArticleDTO(Data Transfer Object):  là thực thể mô tả thông tin của news_article. 
+ * 		Các ArticleDTO này được dùng để trao đổi thông tin giữa lớp Presentation(View) và lớp NewsDAO.
+ */
+
+class Model_ArticleDTO
+{
+protected $_data = array(
+		'article_id' 		 => null,	/** Id of article */
+		'title' 			 => null,	/** Main title of article */
+		'slug' 				 => null,	/** Slug of article */
+		'description' 		 => null,	/** Description of article */	
+		'content' 			 => null,	/** Content of article */
+		'icons' 			 => null,	/** Article icons */
+	
+		/**
+		 * URL of thumbnail image that represent article
+		 */
+		'image_square' 		 => null,	/** The thumbnail in square size */
+		'image_thumbnail' 	 => null,	/** Thumbnail size */
+		'image_small' 		 => null,	/** Small size */
+		'image_crop' 		 => null,	/** Crop size */
+		'image_medium' 		 => null,	/** Medium size */
+		'image_large' 		 => null,	/** Large size */
+	
+		/** 
+		 * Article's status. Can be one of following values:
+		 * - active
+		 * - inactive
+		 * - draft
+		 * - deleted
+		 */
+		'status' 			 => 'inactive',
+		
+		'num_views' 		 => 0,		/** Number of views */
+		
+		'created_date' 		 => null,	/** Article's creation date */
+		'created_user_id' 	 => null,	/** Id of user who create article */
+		'created_user_name'  => null,	/** Username of user who create article */
+
+		'updated_date' 		 => null,	/** Article's modification date */
+		'updated_user_id' 	 => null,	/** Id of user who update article */
+		'updated_user_name'  => null,	/** Username of user who update article */
+	
+		'activate_date' 	 => null,	/** Article's activation date */
+		'activate_user_id' 	 => null,	/** Id of user who activate article */
+		'activate_user_name' => null,	/** Username of user who activate article */
+		
+		'author'			 => null,	/** Author of article */
+		'allow_comment' 	 => 0,		/** Defines that user can comment on article or not */
+		'is_hot'             => 0,      /** Defines that article is hot news or not */
+		'sticky' 			 => 0,		/** Defines that article is sticky of main category or not */
+		'language'			 => null,	/** Language of article (@since 2.0.8) */
+	);
+	
+	public function set($name, $value)
+	{
+		$this->_data[$name] = $value;
+	}
+	
+	public function get($name) 
+	{
+		return $this->_data[$name];;
+	}
+	
+	public function setData( $data)
+	{
+		$this->_data= $data;
+	}
+	
+	public function getData() 
+	{
+		return $this->_data;
+	}
+	
+}
