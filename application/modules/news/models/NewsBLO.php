35a96c1134d7e516c7550f81bf7df7a2721a20a8
greatpearl22@gmail.com <greatpearl22@gmail.com@ce5f66a6-c26f-081e-5279-c115e52cd7a7>
2012-01-13 15:21:56 +0000

    Commit modules News final

---
+++ application/modules/news/models/NewsBLO.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @author Nguyen Ngoc Dai.
+ * @uses NewsBLO(Business Logic Object): Lớp này thực hiện các nghiệp vụ chính của module News, 
+ * 	sử dụng các dịch vụ do lớp NewsDAO(Data Access) cung cấp, 
+ * 	và cung cấp các dịch vụ cho lớp Presentation(View)
+ */
+
+Class Model_NewsBLO {
+	
+	private   $_newsDAO;
+	
+	public function __construct()
+	{
+		$this->_newsDAO = new Model_NewsDAO();
+	}
+
+	public function getById($id)
+	{
+		return $this->_newsDAO->getById($id);
+	}
+	
+	public function getComments($id) {
+		return $this->_newsDAO->getComments($id);
+	}
+	
+	public function getAll()
+	{
+		return $this->_newsDAO->getAll();
+	}
+	
+	public function changeStatus($id,$status)
+	{
+		return $this->_newsDAO->changeStatus($id, $status);
+	}
+
+	public function addNews($article)
+	{
+		$id = $this->_newsDAO->addNews($article);
+		return $id;
+	}
+	
+	public function deleteNews($id)
+	{
+		$this->_newsDAO->deleteNews($id);
+	}
+		
+	public function updateNews($article)
+	{
+		$this->_newsDAO->updateNews($article);
+	}
+	
+	public function editTitle($id,$title)
+	{
+		$this->_newsDAO->editTitle($id, $title);
+	}
+	
+	public function editDescription($id,$description)
+	{
+		$this->_newsDAO->editTitle($id, $description);
+	}
+	
+	public function editAuthor($id,$author)
+	{
+		$this->_newsDAO->editTitle($id, $author);
+	}
+	
+	
+}
